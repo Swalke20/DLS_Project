71d9e18202764a48ccf44b51fb2dfb1546e2f355
Swalke20 <swalke20@student.bbk.ac.uk>
2023-10-10 06:59:10 +0100

    Took out removal of column

--- Data_DLS_Wiki.py
+++ Data_DLS_Wiki.py
@@ -31,16 +31,15 @@ df = pd.concat(frames)
 
 print(df.iloc[0])
 print(len(dud_files))
-full_csv = df.to_csv('full_csv')
+full_csv = df.to_csv('full.csv')
 
 wiki_df = df[['name','role']]
 wiki_df.rename(columns={'fullname': 'Name', 'role': 'Playing role'}, inplace=True)
 print(wiki_df.shape)
 wiki_df = wiki_df.dropna()
 print(wiki_df.shape)
-wiki_df = wiki_df.drop('Unnamed: 0', axis=1)
-print(wiki_df)
-wiki_df.to_csv('wiki_cricketers_csv')
+print(wiki_df.head())
+wiki_df.to_csv('wiki_cricketers.csv')
 
 dud_df = pd.DataFrame(dud_files)
-dud_csv = dud_df.to_csv('dud_csv')
+dud_csv = dud_df.to_csv('dud.csv')
