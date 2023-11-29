0c458925a64c170db0334d6f2eb62bbcffbab13f
Swalke20 <swalke20@student.bbk.ac.uk>
2023-11-29 18:56:30 +0000

    Updating y values to categories

--- data_formatting.py
+++ data_formatting.py
@@ -1,5 +1,6 @@
 #Takes in dataframes and parameters and returns train, test and validation set X and Y as dataframes.
 #Returns a smaller sample if sample and proportion are given.  If proportion 2 then sample is 1/2
+#Converts dependent/ target variable into category for models
 
 def data_format(train_df, test_df, val_df, sample, proportion):
     try:
@@ -20,11 +21,12 @@ def data_format(train_df, test_df, val_df, sample, proportion):
         val_df = val_df.sample(n=val_rows//proportion, replace=False, random_state=7, axis=0)
 
     X_train = train_df.drop('Winner_num',axis=1)
-    y_train = train_df['Winner_num']
+    y_train = train_df['Winner_num'].astype('category')
 
     X_test= test_df.drop('Winner_num',axis=1) 
-    y_test = test_df['Winner_num']
+    y_test = test_df['Winner_num'].astype('category')
 
     X_val= val_df.drop('Winner_num',axis=1) 
-    y_val = val_df['Winner_num']
+    y_val = val_df['Winner_num'].astype('category')
+
     return(X_train, X_test, X_val, y_train, y_test, y_val)
